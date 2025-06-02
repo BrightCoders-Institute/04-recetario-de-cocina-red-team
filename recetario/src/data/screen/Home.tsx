0dd4620e5271f7e95e6f1d29ef492caa4b8d6aa6
Andrea Eunice <andreaeunice2205@gmail.com>
2025-06-02 13:28:30 -0600

    feat: add search bar, install vectorIcons and add search styles

--- recetario/src/data/screen/Home.tsx
+++ recetario/src/data/screen/Home.tsx
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.search}>
+      <Ionicons name="search" size={17} color="black"></Ionicons>
+      <TextInput
+        style={styles.input}
+        placeholder="What do you want to eat?"
+        //onChangeText={handleSearchTextChange}
+        //value={searchText}
+        />
+        <Ionicons name="mic" size={20} color="black"></Ionicons>
+      </View>
       <Text>Home</Text>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+
+  search: {
+    marginBottom: 15,
+    flexDirection: "row",
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+
+  input: {
+    flex: 1,
+    height: 40,
+    padding: 10,
+    fontSize: 17,
+  }
+});
 
 export default Home
+
+// codigo copiado
+/*import React, { useState } from 'react';
+import { View, TextInput, FlatList, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [data, setData] = useState([
+    { id: 1, name: 'Manzana' },
+    { id: 2, name: 'Plátano' },
+    { id: 3, name: 'Naranja' },
+    { id: 4, name: 'Uva' },
+  ]);
+  const [filteredData, setFilteredData] = useState(data);
+
+  const handleSearchTextChange = (text) => {
+    setSearchText(text);
+    const filtered = data.filter((item) =>
+      item.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Buscar"
+        onChangeText={handleSearchTextChange}
+        value={searchText}
+      />
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray',
+  },
+});
+
+export default App;
+*/
