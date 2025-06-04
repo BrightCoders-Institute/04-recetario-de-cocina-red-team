40891a915ce86f884da7c39f99dea999a0c2ba30
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-04 14:59:45 -0600

    feat: add stack screen Details

--- recetario/src/navigation/AppNavigator.tsx
+++ recetario/src/navigation/AppNavigator.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../data/screen/Home';
+import Details from '../data/screen/Details';
 import {Recipes} from "../types/recipes";
 
 export type RootStackParamList = {
@@ -17,6 +18,11 @@ const AppNavigator = () => {
           name="Home"
           component={Home}
           options={{title: 'Home'}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{title: 'Details'}}
         />
       </Stack.Navigator>
   );
