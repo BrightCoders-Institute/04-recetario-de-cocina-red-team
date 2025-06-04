959af9f539d4faa4988df37ed1bcb6ad705d6b27
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-04 14:55:03 -0600

    style: testing recipe's cards style

--- recetario/src/components/recipeCard.tsx
+++ recetario/src/components/recipeCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Text, StyleSheet} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import {Recipes} from "../types/recipes"
 import { RootStackParamList } from "../navigation/AppNavigator";
@@ -7,17 +7,30 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 
 
-export default function recipeCard({recipes} : {recipes:Recipes}) {
+export default function RecipeCard({recipes} : {recipes:Recipes}) {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   const handleDetailPress = () => {
     navigation.navigate("Details", {recipes})
   };
 
+  const styles = StyleSheet.create({
+    button: {
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  }
+});
   return(
-    <TouchableOpacity 
-    onPress={handleDetailPress}
-    />
+    <TouchableOpacity style={styles.button}
+    onPress={handleDetailPress}>
+        <Text>{recipes.name}</Text>
+    </TouchableOpacity>
+
+    
   );
+
 }
  
