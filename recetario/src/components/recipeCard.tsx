3dbf6a22df424bea4e1421ca81ab169223e3c7e7
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-04 19:10:33 -0600

    feat: add images from getImage

--- recetario/src/components/recipeCard.tsx
+++ recetario/src/components/recipeCard.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet} from "react-native";
+import { TouchableOpacity, Text, StyleSheet, Image, View} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import {Recipes} from "../types/recipes"
 import { RootStackParamList } from "../navigation/AppNavigator";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
+import { getImage } from "../utils/getImage";
 
 
 export default function RecipeCard({recipes} : {recipes:Recipes}) {
@@ -19,17 +19,23 @@ export default function RecipeCard({recipes} : {recipes:Recipes}) {
     width: 150,
     height: 150,
     alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
+    borderRadius: 20,
+    marginRight: 10,
+    backgroundColor: '#C4E1E6'
+  },
+  images: {
+    width:120,
+    height: 100,
+    marginVertical:10,
+    borderRadius: 10
   }
 });
   return(
     <TouchableOpacity style={styles.button}
     onPress={handleDetailPress}>
+        <Image source={getImage(recipes.image)} style={styles.images}/>
         <Text>{recipes.name}</Text>
     </TouchableOpacity>
-
-    
   );
 
 }
