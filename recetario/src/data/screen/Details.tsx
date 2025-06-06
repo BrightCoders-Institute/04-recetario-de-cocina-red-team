d1133ae76ec84895324b86543e42f726a51e8377
Andrea Eunice <andreaeunice2205@gmail.com>
2025-06-06 12:04:44 -0600

    feat: add recips params to detalls screen

--- recetario/src/data/screen/Details.tsx
+++ recetario/src/data/screen/Details.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { RootStackParamList } from "../../navigation/AppNavigator";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
 import { getImage } from "../../utils/getImage";
 type DetailsRouteProp = RouteProp<RootStackParamList, "Details">;
 
@@ -12,18 +12,28 @@ const Details = () => {
   return (
     <View style={styles.container}>
       <Image source={getImage(recipes.image)} style={styles.backgroundImage} />
+      <Text>{recipes.category}</Text>
       <Text>{recipes.name}</Text>
+      <Text>Ingredients</Text>
+      <Text>for {recipes.servings} servings</Text>
+       <FlatList
+                data={recipes.ingredients}
+                keyExtractor={(item,index) =>index.toString()}
+                renderItem={({ item }) => (<Text style={styles.ingredients}> {item.name} {item.quantity}</Text>)}
+                />
     </View>
   );
 };
 export default Details;
+
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#393E46",
     flex: 1,
-
     // flexDirection: "column",
   },
+
   backgroundImage: {
     // objectFit: "contain",
     resizeMode: "cover",
@@ -33,4 +43,9 @@ const styles = StyleSheet.create({
     width: "100%",
     // height: "100%",
   },
+
+  ingredients: {
+    marginBottom: 10,
+  }
+
 });
