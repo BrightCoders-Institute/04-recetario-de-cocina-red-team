17f409e5da4cf37d9355885ad447e314cb36a3ae
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-07 12:21:39 -0600

    feat: create RecentList component

---
+++ recetario/src/components/RecentList.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React from "react";
+import recipesData from "../data/recipes.json"
+import RecipeCard from "./recipeCard";
+
+const RecentList = () => {
+  const filterRecent = recipesData.filter((p) => p.category === "recent");
+    
+    return(
+        <View>
+            <Text style={styles.title}>RECENT</Text>
+              <FlatList
+                horizontal={true}
+                data={filterRecent}
+                keyExtractor={(item: any) => item.id}
+                renderItem={({ item }) => <RecipeCard recipes={item} />}
+              />
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+  
+  title: {
+    fontSize: 30,
+    fontWeight: 400,
+    color: "#AB1F60",
+    marginVertical: 20,
+  }
+});
+
+export default RecentList;
