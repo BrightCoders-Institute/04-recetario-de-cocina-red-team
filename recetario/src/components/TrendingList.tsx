301923c5ca2e2e544152386bfbac97ddd39c4f00
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-07 12:30:17 -0600

    feat: create TrendingList component

---
+++ recetario/src/components/TrendingList.tsx
@@ -0,0 +1,32 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React from "react";
+import recipesData from "../data/recipes.json"
+import RecipeCard from "./recipeCard";
+
+const TrendingList = () => {
+
+    const filterTrending = recipesData.filter((p) => p.category === "trending");
+
+    return(
+        <View>
+            <Text style={styles.title}>TRENDING</Text>
+            <FlatList
+                horizontal={true}
+                data={filterTrending}
+                keyExtractor={(item: any) => item.id}
+                renderItem={({ item }) => <RecipeCard recipes={item} />}
+        />
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+  
+  title: {
+    fontSize: 30,
+    fontWeight: 400,
+    color: "#AB1F60",
+    marginVertical: 20,
+  }
+});
+
+export default TrendingList;
