6a7960451058f9be9acceb7d70ea820bd0c7761c
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-07 12:21:09 -0600

    feat: create DetailsList component

---
+++ recetario/src/components/DetailsList.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { RootStackParamList } from "../navigation/AppNavigator";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
+import { getImage } from "../utils/getImage";
+
+type DetailsRouteProp = RouteProp<RootStackParamList, "Details">;
+
+const DetailsList = () => {
+  const route = useRoute<DetailsRouteProp>();
+  const { recipes } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Image source={getImage(recipes.image)} style={styles.backgroundImage} />
+      <Text>{recipes.category}</Text>
+      <Text>{recipes.name}</Text>
+      <Text>Ingredients</Text>
+      <Text>for {recipes.servings} servings</Text>
+       <FlatList
+                data={recipes.ingredients}
+                keyExtractor={(item,index) =>index.toString()}
+                renderItem={({ item }) => (<Text style={styles.ingredients}> {item.name} {item.quantity}</Text>)}
+        />
+    </View>
+  );
+};
+export default DetailsList;
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#393E46",
+    flex: 1,
+  },
+
+  backgroundImage: {
+    resizeMode: "cover",
+    opacity: 0.5,
+    height: "50%",
+    width: "100%",
+  },
+  ingredients: {
+    marginBottom: 10,
+  }
+
+});
