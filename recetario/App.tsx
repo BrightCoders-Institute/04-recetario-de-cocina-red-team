9b297067fec522d416934362dbad02e396000139
Estefania Lopez Dueñas <estefanialopezduenas@gmail.com>
2025-06-09 11:40:08 -0600

    fix: delete extra styles code from Home

--- recetario/App.tsx
+++ recetario/App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 
@@ -10,12 +9,3 @@ export default function App() {
   
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
